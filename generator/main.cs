774c2e44d1a5524cc13682622e5c635e47d9fbba
Christophe VG <contact@christophe.vg>
2017-01-26 13:41:47 +0100

    added HPG BNF Model to example runner

--- generator/main.cs
+++ generator/main.cs
@@ -76,9 +76,163 @@ public class Runner {
       }
     };
   }
+  
+  public static Model CreateBNFGrammar() {
+    // grammar                 ::= { rule } ;
+    // rule                    ::= identifier  "::=" expression ";" ;
+    // expression              ::= identifier-expression
+    //                           | string-expression
+    //                           | extractor-expression
+    //                           | optional-expression
+    //                           | repetition-expression
+    //                           | group-expression
+    //                           | alternatives-expression
+    //                           | sequence-expression
+    //                           ;
+    // identifier-expression   ::= identifier ;
+    // string-expression       ::= string ;
+    // extractor-expression    ::= "/" regex "/"
+    // optional-expression     ::= "[" expression "]" ;
+    // repetition-expression   ::= "{" expression "}" ;
+    // group-expression        ::= "(" expression ")" ;
+    // alternatives-expression ::= expression "|" expression ;
+    // sequence-expression     ::= expression     expression ;
+    //
+    // identifier              ::= /([A-Z][A-Z0-9]*)/ ;
+    // string                  ::= /"([^"]*)"|'([^']*)'/ ;
+    // regex                   ::= /([^/]*)/
+
+    return new Model()  {
+      Rules = new List<Rule>() {
+        new Rule() {
+          Id  = "grammar",
+          Exp = new RepetitionExpression() {
+            Exp = new IdentifierExpression() { Id = "rule" },
+          }
+        },
+        new Rule() {
+          Id  = "rule",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new IdentifierExpression() { Id     = "identifier" },
+              new StringExpression()     { String = "::="        },
+              new IdentifierExpression() { Id     = "expression" },
+              new StringExpression()     { String = ";"          }
+            }
+          }
+        },
+        new Rule() {
+          Id = "expression",
+          Exp = new AlternativesExpression() {
+            Expressions = new List<Expression> {
+              new IdentifierExpression() { Id = "identifier-expression"   },
+              new IdentifierExpression() { Id = "string-expression"       },
+              new IdentifierExpression() { Id = "extractor-expression"    },
+              new IdentifierExpression() { Id = "optional-expression"     },
+              new IdentifierExpression() { Id = "repetition-expression"   },
+              new IdentifierExpression() { Id = "group-expression"        },
+              new IdentifierExpression() { Id = "alternatives-expression" },
+              new IdentifierExpression() { Id = "sequence-expression"     }
+            }
+          }
+        },
+        new Rule() {
+          Id = "identifier-expression",
+          Exp = new IdentifierExpression() { Id = "identifier" }
+        },
+        new Rule() {
+          Id = "string-expression",
+          Exp = new IdentifierExpression() { Id = "string" }
+        },
+        new Rule() {
+          Id = "extractor-expression",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new StringExpression()     { String = "/"          },
+              new IdentifierExpression() { Id     = "extractor" },
+              new StringExpression()     { String = "/"          }
+            }
+          }
+        },
+        new Rule() {
+          Id = "optional-expression",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new StringExpression()     { String = "["          },
+              new IdentifierExpression() { Id     = "expression" },
+              new StringExpression()     { String = "]"          }
+            }
+          }
+        },
+        new Rule() {
+          Id = "repetition-expression",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new StringExpression()     { String = "{"          },
+              new IdentifierExpression() { Id     = "expression" },
+              new StringExpression()     { String = "}"          }
+            }
+          }
+        },
+        new Rule() {
+          Id = "group-expression",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new StringExpression()     { String = "("          },
+              new IdentifierExpression() { Id     = "expression" },
+              new StringExpression()     { String = ")"          }
+            }
+          }
+        },
+        new Rule() {
+          Id = "alternatives-expression",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new IdentifierExpression() { Id     = "expression" },
+              new StringExpression()     { String = "|"          },
+              new IdentifierExpression() { Id     = "expression" }
+            }
+          }
+        },
+        new Rule() {
+          Id = "sequence-expression",
+          Exp = new SequenceExpression() {
+            Expressions = new List<Expression> {
+              new IdentifierExpression() { Id     = "expression" },
+              new IdentifierExpression() { Id     = "expression" }
+            }
+          }
+        },
+        new Rule() {
+          Id  = "identifier",
+          Exp = new Extractor() { Pattern = "([A-Z][A-Z0-9]*)"             }
+        },
+        new Rule() {
+          Id  = "string",
+          Exp = new Extractor() { Pattern = "\\\"([^\\\"]*)\\\"|'([^']*)'" }
+        },
+        new Rule() {
+          Id  = "extractor",
+          Exp = new Extractor() { Pattern = "([^/]*)"                      }
+        }
+      }
+    };
+  }
 
   public static void Main(string[] args) {
-    Model grammar = CreatePascalGrammar();
+
+    var grammarName = "pascal"; // default
+    if(args.Length == 1) {
+      grammarName = args[0];
+    }
+
+    Model grammar;
+
+    switch(grammarName) {
+      case "bnf": grammar = CreateBNFGrammar();    break;
+      default:    grammar = CreatePascalGrammar(); break;
+    }
+
     Parser.Model model = new Parser.Model().Import(grammar);
 
     // Console.WriteLine(model.ToString());

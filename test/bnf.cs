20f44b30f2f197d758ce4a14e9e16aac3a8a9cf9
Christophe VG <contact@christophe.vg>
2017-01-27 22:00:56 +0100

    added alternative string-based unit test helper

--- test/bnf.cs
+++ test/bnf.cs
@@ -6,45 +6,48 @@ using NUnit.Framework;
 
 using System.Collections.Generic;
 
+[TestFixture]
+public class Syntax {
+  Parser parser;
 
-namespace HumanParserGenerator.Test {
+  [SetUp]
+  public void SetUp() {
+    this.parser = new Parser();
+  }
 
-  [TestFixture]
-  public class Syntax {
-    Parser parser;
+  [TearDown]
+  public void TearDown() {
+    this.parser = null;
+  }
 
-    [SetUp]
-    public void SetUp() {
-      this.parser = new Parser();
-    }
+  private void parseAndCompare(string src, Grammar expected) {
+    Assert.AreEqual(
+      expected.ToString(),
+      this.parser.Parse(src).AST.ToString()
+    );
+  }
 
-    [TearDown]
-    public void TearDown() {
-      this.parser = null;
-    }
+  private void parseAndCompare(string src, string expected) {
+    Assert.AreEqual(
+      expected,
+      this.parser.Parse(src).AST.ToString()
+    );
+  }
 
-    private void parseAndCompare(string src, Grammar expected) {
-      Assert.AreEqual(
-        expected.ToString(),
-        this.parser.Parse(src).AST.ToString()
-      );
-    }
-
-    [Test]
-    public void SingleCharacterRule() {
-      this.parseAndCompare(
-        "r ::= \"a\";",
-        new Grammar() {
-          Rules = new List<Rule>() {
-            new Rule() {
-              Identifier     = "r",
-              ExpressionList = new ExpressionList() {
-                Value= new SequenceExpression() {
-                  Expressions = new List<Expression>() {
-                    new Expression() {
-                      Value = new StringExpression() {
-                        String = "a"
-                      }
+  [Test]
+  public void SingleCharacterRule() {
+    this.parseAndCompare(
+      "r ::= \"a\";",
+      new Grammar() {
+        Rules = new List<Rule>() {
+          new Rule() {
+            Identifier     = "r",
+            ExpressionList = new ExpressionList() {
+              Value= new SequenceExpression() {
+                Expressions = new List<Expression>() {
+                  new Expression() {
+                    Value = new StringExpression() {
+                      String = "a"
                     }
                   }
                 }
@@ -52,8 +55,27 @@ namespace HumanParserGenerator.Test {
             }
           }
         }
-      );
-    }
+      }
+    );
+  }
+
+  [Test]
+  public void TwoAlternativeCharactersRule() {
+    this.parseAndCompare(
+      "r ::= \"a\" | \"b\";",
+      "Grammar(" +
+        "Rules=[" +
+          "Rule(" +
+            "Identifier=r,"+
+            "ExpressionList=ExpressionList(" +
+              "Value=AlternativesExpression(" +
+                "Expression="+
+                  "Expression(Value=StringExpression(String=a)),"+
+                  "ExpressionList=ExpressionList(" +
+                    "Value=SequenceExpression(" +
+                    "Expressions=["+
+                      "Expression(Value=StringExpression(String=b))])))))])"
+    );
   }
 
 }

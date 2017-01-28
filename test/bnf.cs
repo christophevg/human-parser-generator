bd2ac52fadb1de88599b81c239b01a36b9049be3
Christophe VG <contact@christophe.vg>
2017-01-29 00:50:01 +0100

    added BNF-self-parsing test (fails due to regex)

--- test/bnf.cs
+++ test/bnf.cs
@@ -111,14 +111,108 @@ public class Syntax {
     );
   }
 
-  // Future ;-)
-  // [Test]
-  // public void BNFSelfHostingTest() {
-  //   this.parseAndCompare(
-  //     System.IO.File.ReadAllText("../grammars/hpg.bnf"),
-  //     HumanParserGenerator.Grammars.AsModel.BNF
-  //   );
-  // }
+  [Test]
+  public void BNFSelfHostingTest() {
+    this.parseAndCompare(
+      System.IO.File.ReadAllText("../../grammars/hpg.bnf"),
+      @"Grammar(
+        Rules=[
+          Rule(Identifier=grammar,
+            Expressions=SequentialExpressions(Expressions=[
+              RepetitionExpression(
+                Expressions=SequentialExpressions(Expressions=[
+                  IdentifierExpression(Identifier=rule)
+                ]))
+            ])),
+          Rule(Identifier=rule,
+            Expressions=SequentialExpressions(Expressions=[
+              IdentifierExpression(Identifier=identifier),
+              StringExpression(String=::=),
+              IdentifierExpression(Identifier=expressions),
+              StringExpression(String=;)
+            ])),
+          Rule(Identifier=expressions,
+            Expressions=AlternativeExpressions(
+              Expression=IdentifierExpression(Identifier=alternative-expressions),
+              Expressions=SequentialExpressions(Expressions=[
+                IdentifierExpression(Identifier=sequential-expressions)
+              ]))),
+          Rule(Identifier=alternative-expressions,
+            Expressions=SequentialExpressions(Expressions=[
+              IdentifierExpression(Identifier=expression),
+              StringExpression(String=|),
+              IdentifierExpression(Identifier=expressions)
+            ])),
+          Rule(Identifier=sequential-expressions,
+            Expressions=SequentialExpressions(Expressions=[
+              RepetitionExpression(Expressions=
+                SequentialExpressions(Expressions=[
+                  IdentifierExpression(Identifier=expression)
+                ]))
+            ])),
+          Rule(Identifier=expression,
+            Expressions=AlternativeExpressions(
+              Expression=IdentifierExpression(Identifier=string-expression),
+              Expressions=AlternativeExpressions(
+                Expression=IdentifierExpression(Identifier=extractor-expression),
+                Expressions=AlternativeExpressions(
+                  Expression=IdentifierExpression(Identifier=optional-expression),
+                  Expressions=AlternativeExpressions(
+                    Expression=IdentifierExpression(Identifier=repetition-expression),
+                    Expressions=AlternativeExpressions(
+                      Expression=IdentifierExpression(Identifier=group-expression),
+                      Expressions=AlternativeExpressions(
+                        Expression=IdentifierExpression(Identifier=or-expression),
+                        Expressions=SequentialExpressions(Expressions=[
+                          IdentifierExpression(Identifier=identifier-expression)
+                        ])))))))),
+          Rule(Identifier=string-expression,
+            Expressions=SequentialExpressions(Expressions=[
+              IdentifierExpression(Identifier=string)
+            ])),
+          Rule(Identifier=extractor-expression,
+            Expressions=SequentialExpressions(Expressions=[
+              StringExpression(String=/),
+              IdentifierExpression(Identifier=regex),
+              StringExpression(String=/)
+            ])),
+          Rule(Identifier=optional-expression,
+            Expressions=SequentialExpressions(Expressions=[
+              StringExpression(String=[),
+              IdentifierExpression(Identifier=expressions),
+              StringExpression(String=])
+            ])),
+          Rule(Identifier=repetition-expression,
+            Expressions=SequentialExpressions(Expressions=[
+              StringExpression(String={),
+              IdentifierExpression(Identifier=expressions),
+              StringExpression(String=})
+            ])),
+          Rule(Identifier=group-expression,
+            Expressions=SequentialExpressions(Expressions=[
+              StringExpression(String=(),
+              IdentifierExpression(Identifier=expressions),
+              StringExpression(String=))
+            ])),
+          Rule(Identifier=identifier-expression,
+            Expressions=SequentialExpressions(Expressions=[
+              IdentifierExpression(Identifier=identifier)
+            ])),
+          Rule(Identifier=identifier,
+            Expressions=SequentialExpressions(Expressions=[
+              ExtractorExpression(Extractor=([A-Za-z][A-Z0-9a-z-]*))
+            ])),
+          Rule(Identifier=string,
+            Expressions=SequentialExpressions(Expressions=[
+              ExtractorExpression(Extractor=""([^""]*)""|^'([^']*)')
+            ])),
+          Rule(Identifier=extractor,
+            Expressions=SequentialExpressions(Expressions=[
+              ExtractorExpression(Extractor=([^/]*)
+            ]))
+        ])"
+    );
+  }
 }
 
 

101bfed6231e1616fc9594f6de6cb013925ebdce
Christophe VG <contact@christophe.vg>
2017-02-03 15:41:18 +0100

    updated bootstrap to new interface

--- generator/bootstrap.cs
+++ generator/bootstrap.cs
@@ -13,11 +13,11 @@ namespace HumanParserGenerator {
     public static void Main(string[] args) {
 
       Grammar grammar = AsModel.BNF;
-      Generator.Model model = new Generator.Model().Import(grammar);
+      Generator.Model model = new Generator.Factory().Import(grammar).Model;
 
       // outputs the intermediate Parser Model
-      // Console.WriteLine(model.ToString());
-      // Console.WriteLine();
+      Console.Error.WriteLine(model.ToString());
+      Console.Error.WriteLine();
 
       Emitter.CSharp code = new Emitter.CSharp().Generate(model);
       Console.WriteLine(code.ToString());

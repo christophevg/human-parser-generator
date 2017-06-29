e29298825a199d0cd825dccf1964659190dffc42
Christophe VG <contact@christophe.vg>
2017-06-29 10:46:01 +0200

    fixed not detecting SPACES

--- example/cobol/test_records.cs
+++ example/cobol/test_records.cs
@@ -149,4 +149,29 @@ public class GeneratorModelTests {
     );
   }
 
+  [Test]
+  public void BasicRecordWithSpaces() {
+    this.testSingleSentence(
+      "01 SOME-FIELD PIC X(16) VALUE SPACES.",
+      new BasicRecord() {
+        Level = new Int() { Value = "01" },
+        LevelName = new LevelName() {
+          HasFiller = false,
+          Identifier = new Identifier() { Name = "SOME-FIELD" },
+        },
+        Options = new List<Option>() {
+          new PictureFormatOption() {
+            Type = "X",
+            Digits = new Int() { Value = "16" },
+            DecimalType = null,
+            DecimalDigits = null
+          },
+          new ValueOption() {
+            HasIs = false,
+            Value = new Space() { HasS = true }
+          }
+        }
+      }
+    );
+  }
 }
